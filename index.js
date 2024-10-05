39d71a1bf2288099a5b19e1a9b3240d605620319
AlexWalker222 <alexw3071@gmail.com>
2024-10-05 11:44:00 -0400

    ""

--- index.js
+++ index.js
@@ -5,8 +5,29 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { MongoClient } = require("mongodb");
-const client = new MongoClient(process.env.DB_URL)
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri, {
+  serverApiL: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // 
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 const db = client.db("urlshortener")
 // Basic Configuration
 const port = process.env.PORT || 3000;
