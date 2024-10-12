e887fb38e9ec6421e5cc8d7da31d542fc894334c
AlexWalker222 <alexw3071@gmail.com>
2024-10-12 15:58:00 -0400

    "

--- index.js
+++ index.js
@@ -1,38 +1,47 @@
 //@ts-nocheck
-'use strict';
 
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = process.env.MONGO_URI;
-const client = new MongoClient(uri, {
-  serverApiL: {
-    version: ServerApiVersion.v1,
+const dns = require("dns");
+app.use(cors({}));
+const { MongoClient } = require('mongodb');
+const client = new MongoClient(process.env.MONGO_URI, {
+  serverApi: {
     strict: true,
-    deprecationErrors: true,
+    unifiedTopology: true,
+    deprecationErrors: true
   }
 });
-async function run() {
+
+function run() {
   try {
-    // connect the client to the server (optional starting in v4.7)
-    await client.connect();
-    // 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // connect the client to the server
+    // (optional starting in v4.7)
+    async function run() {
+
+
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    run().catch(console.dir);
+
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    // Ensures that the client will close
+    // when you finish / error
+    client.close();
   }
 }
 run().catch(console.dir);
-const db = client.db("urlshortener")
+
+const db = client.db('urlshortener');
+const urls = db.collection('urls');
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -41,25 +50,24 @@ app.use('/public', express.static(`${process.cwd()}/public`));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
-let urls;
+const urlCount = urls.countDocuments({})
 
 // Your first API endpoint
-app.post('/api/shorturl', function (req, res, dns) {
+app.post('/api/shorturl', function (req, res) {
   console.log(req.body);
   const url = req.body.url;
   const dnslookup = dns.lookup(url.parser.parse(url).hostname,
-    async (err, address) => {
-      if (!address) {
+    function (address, err) {
+      if (!address === err) {
         res.json({ error: "Invalid URL" });
       } else {
-        const urlCount = await urls.countDocuments({})
         const urlDoc = {
           url,
           short_url: urlCount
         }
         const result = urls.insertOne(urlDoc);
         console.log(result);
-        res.json({ original_url: url, short_url: urlCount })
+        res.json({ original_url: url, short_url: urlCount });
       }
     });
 });
@@ -71,6 +79,6 @@ app.get("/api/shorturl/:short_url", async (req, res) => {
   res.redirect(urlDoc.url)
 })
 
-app.listen(port, function () {
+app.listen(port, function (req, res) {
   console.log(`Listening on port ${port}`);
 });
